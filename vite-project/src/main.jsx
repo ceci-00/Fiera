2c4e63783e0bccc6ba87f29fc5248152497042fe
ceci-00 <ceci.ga003@gmail.com>
2024-06-04 23:22:17 -0500

    solved root issue not displaying on full page. working on landing page css styling. imported bootstrap but don't know if I want to use it yet

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,5 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import * as bootstrap from 'bootstrap';
 
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
